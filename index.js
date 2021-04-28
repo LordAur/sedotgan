97f316b79ef35710b2e18ff2f888b8c859db4fb0
Yudha Pratama <yudha.webdev@gmail.com>
2021-04-28 11:30:59 +0700

    Init

---
+++ index.js
@@ -0,0 +1,35 @@
+require('dotenv').config()
+const fastify = require('fastify')({ logger: true })
+const qs = require('querystring')
+
+const crawling = require('./lib/crawler')
+
+fastify.get('/crawling', async (request, reply) => {
+    const q = qs.parse(request.url.split("?")[1])
+    const data = JSON.parse(q.data)
+    crawling.crawler(q.url, data)
+        .then((resp) => {
+            reply
+                .code(200)
+                .header('Content-Type', 'application/json; charset=utf-8')
+                .send(resp)
+        })
+        .catch((err) => {
+            reply
+                .code(400)
+                .header('Content-Type', 'application/json; charset=utf-8')
+                .send({
+                    errors: err.message
+                })
+        })
+})
+
+const start = async () => {
+    try {
+      await fastify.listen(process.env.PORT)
+    } catch (err) {
+      fastify.log.error(err)
+      process.exit(1)
+    }
+}
+start()
