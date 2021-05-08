864c177c678444dfd71dbb34cdc9754456118181
Yudha Pratama <yudha.webdev@gmail.com>
2021-05-08 17:01:04 +0700

    Added, support to download zip and make filename sequential

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 require('dotenv').config()
 const fastify = require('fastify')({ logger: true })
 const qs = require('querystring')
+const contentDisposition = require('content-disposition')
 
 const utils = require('./lib/utils')
 const crawling = require('./lib/crawler')
@@ -15,43 +16,56 @@ fastify.get('/crawling', async (request, reply) => {
         data = utils.normalize(q.data)
     }
 
+    let clickBefore
+    if (q.clickBefore !== undefined) {
+        clickBefore = JSON.parse(q.clickBefore)
+        if (typeof clickBefore === "string") {
+            const tmp = utils.normalize(clickBefore)
+            clickBefore = [tmp]
+        } else if (typeof q.clickBefore === "[object Object]" && q.clickBefore.length > 0) {
+            q.clickBefore.map((node, index) => {
+                const tmp = utils.normalize(node)
+                clickBefore[index] = tmp 
+            })
+        }
+    } else clickBefore = []
+
     if (q.puppeteer === "true") {
         let paginateClick = utils.normalize(q.paginateClick)
         let paginateEnd = utils.normalize(q.paginateEnd)
 
-        crawling.domCrawler(q.url, data, Boolean(q.paginate), q.paginateLimit || 1, paginateClick, paginateEnd)
+        crawling.domCrawler(q.url, data, Boolean(q.paginate), q.paginateLimit || 1, paginateClick, paginateEnd, clickBefore)
             .then((resp) => {
-                reply
-                    .code(200)
-                    .header('Content-Type', 'application/json; charset=utf-8')
-                    .send(resp)
+                if (q.download === "true") zipResponse(reply, resp, Boolean(q.sequentialName), utils.normalize(q.filename))
+                else jsonResponse(reply, 200, resp)
             })
-            .catch((err) => {
-                reply
-                    .code(400)
-                    .header('Content-Type', 'application/json; charset=utf-8')
-                    .send({
-                        errors: err.message
-                    })
-            })
+            .catch((err) => jsonResponse(reply, 400, {error: err}))
     } else {
         crawling.crawler(q.url, data)
             .then((resp) => {
-                reply
-                    .code(200)
-                    .header('Content-Type', 'application/json; charset=utf-8')
-                    .send(resp)
+                if (q.download === "true") zipResponse(reply, resp, Boolean(q.sequentialName), utils.normalize(q.filename))
+                else jsonResponse(reply, 200, resp)
             })
-            .catch((err) => {
-                reply
-                    .code(400)
-                    .header('Content-Type', 'application/json; charset=utf-8')
-                    .send({
-                        errors: err.message
-                    })
-            })
+            .catch((err) => jsonResponse(reply, 400, {error: err}))
     }
 })
+
+const jsonResponse = (reply, statusCode, body) => {
+    reply
+        .code(statusCode)
+        .header('Content-Type', 'application/json; charset=utf-8')
+        .send(body)
+}
+
+const zipResponse = async (reply, body, sequentialName, filename) => {
+    const zipBody = await utils.zipBody(body, sequentialName)
+    if (filename === "") filename = "default"
+    reply
+        .code(200)
+        .header('Content-Type', 'application/zip; charset=utf-8')
+        .header('Content-Disposition', contentDisposition(`${filename}.zip`))
+        .send(zipBody)
+}
 
 const start = async () => {
     try {
